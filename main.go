296a9521fd8a7004e37c5797213da954ba1d9a5c
nkpoid <nkpoidjp@gmail.com>
2019-12-18 18:41:15 +0900

    impl

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/nlopes/slack"
+)
+
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("mail2slack must specify a slack token as the first argument")
+	}
+
+	api := slack.New(os.Args[1])
+	_, err := api.UploadFile(
+		slack.FileUploadParameters{
+			Reader:   os.Stdin,
+			Filename: "mail",
+			Filetype: "email",
+			Channels: []string{"#server-mail"},
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
