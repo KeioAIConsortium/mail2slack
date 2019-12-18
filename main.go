fda414185ce7f71c6413e1a4680b37eea69486c6
nkpoid <nkpoidjp@gmail.com>
2019-12-18 20:04:42 +0900

    use syslog

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/nlopes/slack"
+	"log/syslog"
+	"os"
 )
 
 func main() {
+	logger, err := syslog.NewLogger(syslog.LOG_ALERT, 0)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
 	if len(os.Args) < 2 {
-		log.Fatal("mail2slack must specify a slack token as the first argument")
+		logger.Fatal("mail2slack must specify a slack token as the first argument")
 	}
 
 	api := slack.New(os.Args[1])
-	_, err := api.UploadFile(
+	_, err = api.UploadFile(
 		slack.FileUploadParameters{
 			Reader:   os.Stdin,
 			Filename: "mail",
@@ -22,6 +26,6 @@ func main() {
 		},
 	)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 }
